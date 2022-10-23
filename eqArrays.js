1f852a4497a922c8eef05dda46fbc0bf66c7afff
binding1 <jaey.pro@gmail.com>
2022-10-23 18:46:32 +0000

    updated to remove console.logs

--- eqArrays.js
+++ eqArrays.js
@@ -9,11 +9,9 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
-        console.log(false);
         return false;
       } 
     }
-  console.log(true);
   return true;
 }
 
