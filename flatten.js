7e0a4e6f1b4253ea994ae0c12bf32f7fcd385c20
binding1 <jaey.pro@gmail.com>
2022-10-23 18:32:41 +0000

    function that flattens an array

---
+++ flatten.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } 
+    }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const flatten = function(arr) {
+  const flatArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flatArr.push(arr[i][j]);
+      }
+    } else {
+      flatArr.push(arr[i]);
+    }
+  }
+  return flatArr;
+}
+const testArr = [1, 2, [3, 4], 5, [6]]
+flatten(testArr) // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten(testArr), testArr); //should be false, original testArr has nested arrays
