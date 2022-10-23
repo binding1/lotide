11cb074461790846e0bca16a3e466f36d220a90d
binding1 <jaey.pro@gmail.com>
2022-10-23 18:16:02 +0000

    function that takes an array and compares it with a list of elements to be removed, then removes elements from that first array

---
+++ without.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false);
+        return false;
+      } 
+    }
+  console.log(true);
+  return true;
+}
+
+const without = function(source, itemsToRemove) {
+  const resultArr = [...source];
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        resultArr.splice(i, 1);
+      }
+    }
+  }
+  return resultArr;
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
