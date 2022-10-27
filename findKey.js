d78a49fd4fe356105fb918047ef6d88e13d7095a
binding1 <jaey.pro@gmail.com>
2022-10-27 18:30:01 +0000

    function that finds a key based on condition

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = ((obj, callback) => {
+  for (let k in obj) {
+    for (let j in obj[k]) {
+      if (callback(obj[k])) {
+        return k;
+      }
+    }
+  }
+  console.log("here");
+});
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(test1, "noma");
