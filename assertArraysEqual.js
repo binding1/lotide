f3b30d249cb4013efd010b44499dc440b7216b1d
binding1 <jaey.pro@gmail.com>
2022-11-09 07:53:03 +0000

    refactoring using mocha and chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,9 +3,9 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
   }
 };
 
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,12 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, 3],[1, 2, 3]); //true
-assertArraysEqual([1, 2, 3],[3, 2, 1]); //false
+describe('#assertArraysEqual', () => {
+  it("returns assertion passed for asserting [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3],[1, 2, 3]), '✅✅✅ Assertion Passed: 1,2,3 === 1,2,3');
+  });
+
+  it("returns assertion fails for asserting [1, 2, 3] and [3, 2, 1]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3],[3, 2, 1]), '🛑🛑🛑 Assertion Failed: 1,2,3 !== 3,2,1');
+  });
+})
