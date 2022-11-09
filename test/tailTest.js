146614058c407c4bbab8b81e3adbad980084c0c7
binding1 <jaey.pro@gmail.com>
2022-11-09 06:51:35 +0000

    refactored using modules

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const tail = require('../tail.js');
+const assertEqual = require('../assertEqual')
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+console.log(tail([1]));
+console.log(tail([]));
