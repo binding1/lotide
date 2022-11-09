2781812874009ab1dac79157c129eea456aa3814
binding1 <jaey.pro@gmail.com>
2022-11-09 07:19:35 +0000

    refactoring using modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -18,6 +12,8 @@ const eqArrays = function(arr1, arr2) {
     }
   return true;
 };
+
+module.exports = eqArrays;
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
